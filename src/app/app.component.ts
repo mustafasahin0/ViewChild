a48350d0e30041c37c95138832385a6d7fe600db
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-28 22:44:51 -0700

    added demo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ export class AppComponent {
 
   @ViewChild('dobInput') dateOfBirth!: ElementRef;
   @ViewChild('ageInput') age!: ElementRef;
+  @ViewChild(DemoComponent, {static: true}) demoComp!: DemoComponent;
 
   calculateAge(){
     let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
